c4c6b1585f3419c50c5cbdd1c1475c4e6dce2622
ArturiaOS <arturiaevergarden@vip.qq.com>
2024-09-08 23:01:24 -0600

    Updated KawaiiKeyboard

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 
 
 def main():
-    file = open("Library/Documents/Readme.txt", "r")
+    file = open("Library/Documents/RM.txt", "r")
     file_contents = file.read()
     file.close()
     print(file_contents)
